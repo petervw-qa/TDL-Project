7a0661f9493d3359aa5aecddefdc409ca7b56128
Tukanami <67924806+Tukanami@users.noreply.github.com>
2021-02-11 17:03:09 +0000

    TDLIST-12 #created delete functionality for list items controller

--- src/main/java/com/qa/application/controller/ListItemsController.java
+++ src/main/java/com/qa/application/controller/ListItemsController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -48,6 +49,13 @@ public class ListItemsController {
 	@PutMapping("/update/{id}")
 	public ResponseEntity<ListItemsDto> update(@PathVariable Long id, @RequestBody ListItemsDto listItemsDto) {
 		return new ResponseEntity<>(this.service.update(listItemsDto, id), HttpStatus.ACCEPTED);
+	}
+
+//	Delete functionality for controller
+	@DeleteMapping("/delete/{id}")
+	public ResponseEntity<ListItemsDto> delete(@PathVariable Long id) {
+		return this.service.delete(id) ? new ResponseEntity<>(HttpStatus.NO_CONTENT)
+				: new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
 	}
 
 }
