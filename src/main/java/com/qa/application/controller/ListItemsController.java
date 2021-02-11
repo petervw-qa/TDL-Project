a712a390349fb988a1eb4f34ad2d4069b4f65f91
Tukanami <67924806+Tukanami@users.noreply.github.com>
2021-02-11 15:58:53 +0000

    TDLIST-9 #created create functionality for list items controller

--- src/main/java/com/qa/application/controller/ListItemsController.java
+++ src/main/java/com/qa/application/controller/ListItemsController.java
@@ -1,5 +1,30 @@
 package com.qa.application.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import com.qa.application.dto.ListItemsDto;
+import com.qa.application.persistence.domain.ListItems;
+import com.qa.application.service.ListItemsService;
+
 public class ListItemsController {
+
+	private ListItemsService service;
+
+	@Autowired
+	public ListItemsController(ListItemsService service) {
+		super();
+		this.service = service;
+	}
+
+//	Create functionality for Controller
+	@PostMapping("/create")
+	public ResponseEntity<ListItemsDto> create(@RequestBody ListItems listItems) {
+		ListItemsDto created = this.service.create(listItems);
+		return new ResponseEntity<>(created, HttpStatus.CREATED);
+	}
 
 }
