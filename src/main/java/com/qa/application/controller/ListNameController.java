685d1d9f8200096e3d5128355f2d681da99b55fa
Tukanami <67924806+Tukanami@users.noreply.github.com>
2021-02-11 15:01:57 +0000

    TDLIST-4 created controller create functionality in listname controller

--- src/main/java/com/qa/application/controller/ListNameController.java
+++ src/main/java/com/qa/application/controller/ListNameController.java
@@ -1,12 +1,29 @@
 package com.qa.application.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.qa.application.dto.ListNameDto;
+import com.qa.application.persistence.domain.ListName;
+import com.qa.application.service.ListNameService;
 
 @RestController
 @CrossOrigin
 @RequestMapping("/listname")
 public class ListNameController {
+	
+	private ListNameService service;
+	
+//	Create Method for Controller
+	@PostMapping("/create")
+	public ResponseEntity<ListNameDto> create(@RequestBody ListName ListName) {
+		ListName created = this.service.create(ListName);
+		return new ResponseEntity<>(created, HttpStatus.CREATED);
+	}
 
 }
