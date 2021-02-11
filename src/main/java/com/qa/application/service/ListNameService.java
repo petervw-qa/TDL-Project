83727f3a159a8313b050f1a132ace3773db081aa
Tukanami <67924806+Tukanami@users.noreply.github.com>
2021-02-11 15:18:47 +0000

    TDLIST-20 # created update functionality for listname service

--- src/main/java/com/qa/application/service/ListNameService.java
+++ src/main/java/com/qa/application/service/ListNameService.java
@@ -9,40 +9,48 @@ import org.springframework.stereotype.Service;
 import com.qa.application.dto.ListNameDto;
 import com.qa.application.persistence.domain.ListName;
 import com.qa.application.persistence.repo.ListNameRepo;
+import com.qa.application.utils.SpringBeanUtil;
 
 @Service
 public class ListNameService {
-	
+
 	private ListNameRepo repo;
-	
+
 	private ModelMapper mapper;
-	
+
 	private ListNameDto mapToDto(ListName listname) {
 		return this.mapper.map(listname, ListNameDto.class);
 	}
-	
+
 	@Autowired
 	public ListNameService(ListNameRepo repo, ModelMapper mapper) {
 		super();
 		this.repo = repo;
 		this.mapper = mapper;
 	}
-	
+
 	// Create method for Service
-		public ListNameDto create(ListName listName) {
-			return this.mapToDto(this.repo.save(listName));
-		}
-	
-		// Read All
-		public List<ListNameDto> readAll() {
-			return this.repo.findAll().stream().map(this::mapToDto).collect(Collectors.toList());
+	public ListNameDto create(ListName listName) {
+		return this.mapToDto(this.repo.save(listName));
+	}
 
-		}
+	// Read All method for Service
+	public List<ListNameDto> readAll() {
+		return this.repo.findAll().stream().map(this::mapToDto).collect(Collectors.toList());
 
-		// Read by Id
-		public ListNameDto readById(Long id) {
-			return this.mapToDto(this.repo.findById(id).orElseThrow(ListNameNotFoundException::new));
-		}	
-	
+	}
+
+	// Read by ID method for Service
+	public ListNameDto readById(Long id) {
+		return this.mapToDto(this.repo.findById(id).orElseThrow(ListNameNotFoundException::new));
+	}
+
+	// Update method for Service
+	public ListNameDto update(ListNameDto listNameDto, Long id) {
+		ListName toUpdate = this.repo.findById(id).orElseThrow(ListNameNotFoundException::new);
+		toUpdate.setName(listNameDto.getName());
+		SpringBeanUtil.mergeNotNull(listNameDto, toUpdate);
+		return this.mapToDto(this.repo.save(toUpdate));
+	}
 
 }
