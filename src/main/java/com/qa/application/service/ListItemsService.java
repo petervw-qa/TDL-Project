98baf3bf8706fb945752362b6281068f4ea61271
Tukanami <67924806+Tukanami@users.noreply.github.com>
2021-02-11 15:53:38 +0000

    TDLIST-23 #created create method for list items service

--- src/main/java/com/qa/application/service/ListItemsService.java
+++ src/main/java/com/qa/application/service/ListItemsService.java
@@ -1,5 +1,32 @@
 package com.qa.application.service;
 
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.qa.application.dto.ListItemsDto;
+import com.qa.application.persistence.domain.ListItems;
+import com.qa.application.persistence.repo.ListItemsRepo;
+
 public class ListItemsService {
+
+	private ListItemsRepo repo;
+
+	private ModelMapper mapper;
+
+	private ListItemsDto mapToDto(ListItems listItems) {
+		return this.mapper.map(listItems, ListItemsDto.class);
+	}
+
+	@Autowired
+	public ListItemsService(ListItemsRepo repo, ModelMapper mapper) {
+		super();
+		this.repo = repo;
+		this.mapper = mapper;
+	}
+
+	// Create method for Service
+	public ListItemsDto create(ListItems listItems) {
+		return this.mapToDto(this.repo.save(listItems));
+	}
 
 }
