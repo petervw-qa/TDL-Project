06f585c096d6834f3b2183ace431da3e0ba8d616
Tukanami <67924806+Tukanami@users.noreply.github.com>
2021-02-11 15:25:52 +0000

    TDLIST-7 #created delete functionality for Listname controller

--- src/main/java/com/qa/application/controller/ListNameController.java
+++ src/main/java/com/qa/application/controller/ListNameController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -21,16 +22,16 @@ import com.qa.application.service.ListNameService;
 @CrossOrigin
 @RequestMapping("/listname")
 public class ListNameController {
-	
+
 	private ListNameService service;
-	
+
 //	Create Method for Controller
 	@PostMapping("/create")
 	public ResponseEntity<ListNameDto> create(@RequestBody ListName listName) {
 		ListNameDto created = this.service.create(listName);
 		return new ResponseEntity<>(created, HttpStatus.CREATED);
 	}
-	
+
 //	Read all method for Controller
 	@GetMapping("/read")
 	public ResponseEntity<List<ListNameDto>> readAll() {
@@ -42,11 +43,18 @@ public class ListNameController {
 	public ResponseEntity<ListNameDto> readOne(@PathVariable Long id) {
 		return ResponseEntity.ok(this.service.readById(id));
 	}
-	
+
 //	Update method for Controller
 	@PutMapping("/update/{id}")
 	public ResponseEntity<ListNameDto> update(@PathVariable Long id, @RequestBody ListNameDto listName) {
 		return new ResponseEntity<>(this.service.update(listName, id), HttpStatus.ACCEPTED);
+	}
+
+//	Delete method for Controller
+	@DeleteMapping("/delete/{id}")
+	public ResponseEntity<ListNameDto> delete(@PathVariable Long id) {
+		return this.service.delete(id) ? new ResponseEntity<>(HttpStatus.NO_CONTENT)
+				: new ResponseEntity<>(HttpStatus.I_AM_A_TEAPOT);
 	}
 
 }
