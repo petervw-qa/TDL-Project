91bf66b32c57b013193a389078e3e58369cbe4b5
Tukanami <67924806+Tukanami@users.noreply.github.com>
2021-02-11 15:14:41 +0000

    TDLIST-5 #made read methods for listname controller but requires some debugging in service

--- src/main/java/com/qa/application/controller/ListNameController.java
+++ src/main/java/com/qa/application/controller/ListNameController.java
@@ -1,8 +1,12 @@
 package com.qa.application.controller;
 
+import java.util.List;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -24,6 +28,18 @@ public class ListNameController {
 	public ResponseEntity<ListNameDto> create(@RequestBody ListName ListName) {
 		ListNameDto created = this.service.create(ListName);
 		return new ResponseEntity<>(created, HttpStatus.CREATED);
+	}
+	
+//	Read all method for Controller
+	@GetMapping("/read")
+	public ResponseEntity<List<ListNameDto>> readAll() {
+		return ResponseEntity.ok(this.service.readAll());
+	}
+
+//	Read by ID method for Controller
+	@GetMapping("/read/{id}")
+	public ResponseEntity<ListNameDto> readOne(@PathVariable Long id) {
+		return ResponseEntity.ok(this.service.readById(id));
 	}
 
 }
