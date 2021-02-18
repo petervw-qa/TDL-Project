0c7635258ecb7f49cfb865c85696c212a8b48848
Tukanami <67924806+Tukanami@users.noreply.github.com>
2021-02-18 23:36:27 +0000

    completed listitemscontroller test

--- src/test/java/com/qa/application/rest/ListItemsControllerTest.java
+++ src/test/java/com/qa/application/rest/ListItemsControllerTest.java
@@ -1,5 +1,90 @@
 package com.qa.application.rest;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.atLeastOnce;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import com.qa.application.controller.ListItemsController;
+import com.qa.application.persistence.domain.ListItem;
+import com.qa.application.persistence.dto.ListItemDto;
+import com.qa.application.service.ListItemsService;
+
+@SpringBootTest
 public class ListItemsControllerTest {
+	
+	@MockBean // Mock
+	ListItemsService mockService;
+	@Autowired /// Inject mocks
+	ListItemsController controller;
+	@Autowired
+	ModelMapper mapper;
+	
+	private final ListItem Item_1 = new ListItem("Exercise","Chest Day", false);
+	private final ListItem Item_2 = new ListItem("Shopping","Lidl has the best deals", false);
+	private final List<ListItem> listOfItemss = List.of(Item_1, Item_2);
+	
+	private ListItemDto mapToDto(ListItem listItem) {
+		return this.mapper.map(listItem, ListItemDto.class);
+	}
+	
+	@Test
+	void createTEST() throws Exception {
+		when(this.mockService.create(Item_1)).thenReturn(this.mapToDto(Item_1));
+		assertThat(new ResponseEntity<ListItemDto>(this.mapToDto(Item_1), HttpStatus.CREATED))
+		.isEqualTo(this.controller.create(Item_1));
+		verify(this.mockService, Mockito.times(1)).create(Item_1);
+	}
+	
+	@Test
+	void readAllTEST() throws Exception {
+		List<ListItemDto> itemDtos = listOfItemss.stream().map(this::mapToDto).collect(Collectors.toList());
+		when(this.mockService.readAll()).thenReturn(itemDtos);
+		assertThat(this.controller.readAll()).isEqualTo(new ResponseEntity<>(itemDtos, HttpStatus.OK));
+		verify(this.mockService, atLeastOnce()).readAll();
+	}
+	
+	@Test
+	void readOneTEST() throws Exception {
+		final Long id = 2L;
+		final ListItemDto listItemDto = this.mapToDto(Item_2);
+		when(this.mockService.readById(id)).thenReturn(listItemDto);
+		ResponseEntity<ListItemDto> expected = ResponseEntity.ok(listItemDto);
+		ResponseEntity<ListItemDto> actual = this.controller.readOne(id);
+		assertEquals(expected,actual);
+		verify(this.mockService, times(1)).readById(id);
+	}
+	
+	@Test
+	void updateTEST() throws Exception {
+		final Long id = 1L;
+		final ListItemDto listItemDto = null;
+		when(this.mockService.update(listItemDto, id)).thenReturn(this.mapToDto(Item_1));
+		assertThat(new ResponseEntity<ListItemDto>(this.mapToDto(Item_1), HttpStatus.ACCEPTED))
+				.isEqualTo(this.controller.update(id, listItemDto));
+		verify(this.mockService, atLeastOnce()).update(listItemDto, id);
+	}
+	
+	@Test
+	void deleteTEST() throws Exception {
+		when(this.mockService.delete(Item_2.getId())).thenReturn(false);
+		assertThat(this.controller.delete(Item_2.getId()))
+				.isEqualTo(new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR));
+		verify(this.mockService, atLeastOnce()).delete(Item_2.getId());
+	}
 
 }
