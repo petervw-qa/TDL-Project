35ecee1a8622aa83f4884807bcb8d97434408777
Tukanami <67924806+Tukanami@users.noreply.github.com>
2021-02-18 12:50:10 +0000

    updated crud functionality for listname.js

--- src/main/resources/static/js/ListName.js
+++ src/main/resources/static/js/ListName.js
@@ -1,10 +1,31 @@
 'use strict';
 
-const createListName = () => {
-	const listName = document.querySelector('#listname')
+const listNameName = document.querySelector('#listname-name')
+const listNameID = document.querySelector('#listname-read-id')
+const listNameRename = document.querySelector('#listname-rename')
+const listNameUpdateId = document.querySelector('#listname-update-id')
+const listNameDeleteId = document.querySelector('#listname-delete-id')
+
+const newList = () => {
+	const list = document.createElement("li");
+	const e = document.getElementById("listNameList").value;
+	const text = document.createTextNode(e);
+	list.appendChild(text);
+	if (e === ``) {
+		alert("You can't have an empty list!");
+	} else {
+		document.getElementById("listName-unlisted").appendChild(list);
+	} document.getElementById("listNameList").value = "";
+}
+
+const postListName = () => {
+
+	const lName = listNameName.value;
+
 	const body = {
-		"name": listName.value
+		"name": lName.value
 	}
+	console.log(body)
 	fetch("http://localhost:9092/listname/create", {
 		method: "POST",
 		headers: {
@@ -23,37 +44,48 @@ const createListName = () => {
 		.catch((error) => console.error(`There has been an error. It is: ${error}`))
 };
 
-const readAllListName = () => {
-	fetch("http://localhost:9092/listname/read")
+const getAllListName = () => {
+	fetch(`http://localhost:9092/listname/read`)
 		.then((response) => {
-			(response.status !== 200) ? console.error(`There as been an error: ${response.status}`) :
-				response.json()
-					.then((data) => {
-						console.log(data)
-					})
-		}).catch((error) => console.error(`There has been an error. It is: ${error}`))
-};
+			if (response.status !== 200) {
+				console.log(`There as been an error: ${response.status}`);
+				return;
+			}
+			response.json()
+				.then((data) => console.log(data));
+		}).catch((err) => console.error(`There has been an error. It is: ${err}`));
+}
 
-const readByIDListName = () => {
-	const listNameID = document.querySelector('#listname-read-by-id').value
-	fetch(`http://localhost:9092/listname/read/${listNameID}`)
-		.then((response) => {
-			(response.status !== 200) ? console.error(`There as been an error: ${response.status}`) :
-				response.json()
-					.then((data) => {
-						console.log(data)
-					})
-		}).catch((error) => console.error(`There has been an error. It is: ${error}`))
-};
+const getByIDListName = () => {
+
+    let id = listNameID.value;
+    const params = new URLSearchParams(window.location.search);
+    console.log(params);
+    for (const param of params){
+        console.log(param);
+    }
+    console.log(`This list's ID is: ${id}`);
+    fetch(`http://localhost:9092/listname/read/${listNameID}`)
+    .then((response) => {
+        if(response.status !==200){
+            console.log(`There has been an error. It is: ${error}`);
+            return;
+        }
+        response.json()
+        .then((data) => console.log(data));
+    }) .catch((err) => console.error(`There has been an error. It is: ${err}`));
+}
 
 
-const updateListName = () => {
-	const listNameID = document.querySelector('#listname-update').value
-	const listNameRename = document.querySelector('#listname-rename')
+const putListName = () => {
+
+	const nID = listNameUpdateId.value;
+	const nName = listNameRename.value
+
 	const body = {
-		"name": listNameRename.value
+		"name": nName.value
 	}
-	fetch(`http://localhost:9092/listname/update/${listNameID}`, {
+	fetch(`http://localhost:9092/listname/update/${nID}`, {
 		method: "PUT",
 		headers: {
 			"Content-type": "application/json"
@@ -69,12 +101,12 @@ const updateListName = () => {
 };
 
 const deleteListName = () => {
-	const listNameID = document.querySelector('#listname-delete').value
-	fetch(`http://localhost:9092/listname/delete/${listNameID}`, {
+	const deleteId = listNameDeleteId.value
+	fetch(`http://localhost:9092/listname/delete/${deleteId}`, {
 		method: "DELETE"
 	})
 		.then((response) => {
 			(response.status !== 204) ? console.error(`There as been an error: ${response.status}`) :
 				console.log("Successfully deleted!")
-		})
+		}).catch((err) => console.log(err));
 };
