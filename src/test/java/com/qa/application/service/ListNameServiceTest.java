3115517b04b2e0f657b4b076e20a86eefdbfee1a
Peter Vaughan-Williams <pvaughan-williams@qa.com>
2021-02-20 15:23:31 +0000

    finished listname service tests after backend fix, missing out one branch on delete so far

--- src/test/java/com/qa/application/service/ListNameServiceTest.java
+++ src/test/java/com/qa/application/service/ListNameServiceTest.java
@@ -27,14 +27,16 @@ public class ListNameServiceTest {
 	@Autowired
 	private ListNameService service;
 	@MockBean
-	private ListNameRepo repo;
+	private ListNameRepo mockRepo;
 	@Autowired
 	private ModelMapper mapper;
 
-	private final ListName List_1 = new ListName("Peter's List");
-	private final ListName List_2 = new ListName("Artur's List");
-	private final ListName List_1_Updated = new ListName(null, "To Do List");
+	private final ListName List_1 = new ListName("Monday");
+	private final ListName List_2 = new ListName("Tuesday");
+	
+	private final ListName List_1_Updated = new ListName(null, "Monday Extended");
 	private final ListName List_1_Gets = new ListName(1L, List_1_Updated.getName());
+	
 	private final List<ListName> listOfLists = List.of(List_1, List_2);
 
 	private ListNameDto mapToDto(ListName listName) {
@@ -43,49 +45,55 @@ public class ListNameServiceTest {
 
 	@Test
 	void createTEST() throws Exception {
-		final ListName List_No_Id = new ListName(null, "Peter's List");
-		final ListName List_With_Id = new ListName(1L, "Peter's List");
-		when(this.repo.save(List_No_Id)).thenReturn(List_With_Id);
-		assertThat(this.service.create(List_No_Id)).isEqualTo(this.mapToDto(List_With_Id));
-		verify(this.repo, atLeastOnce()).save(List_No_Id);
+		final ListName List_No_Id = new ListName(null, "Monday");
+		final ListName List_With_Id = new ListName(1L, "Monday");
+		when(this.mockRepo.save(List_No_Id)).thenReturn(List_With_Id);
+		assertThat(this.service.create(List_No_Id))
+		.isEqualTo(this.mapToDto(List_With_Id));
+		verify(this.mockRepo, atLeastOnce()).save(List_No_Id);
 	}
 
 	@Test
 	void readAllTEST() throws Exception {
 		List<ListNameDto> nameDtos = listOfLists.stream().map(this::mapToDto).collect(Collectors.toList());
-		when(this.repo.findAll()).thenReturn(listOfLists);
-		assertThat(this.service.readAll()).isEqualTo(nameDtos);
-		verify(this.repo, atLeastOnce()).findAll();
-
+		when(this.mockRepo.findAll()).thenReturn(listOfLists);
+		assertThat(this.service.readAll())
+		.isEqualTo(nameDtos);
+		verify(this.mockRepo, atLeastOnce()).findAll();
 	}
 
 	@Test
 	void readByIdTEST() throws Exception {
 		final Long id = 2L;
-		ListName Test_List_2 = new ListName(id, "Artur's List");
-		when(this.repo.findById(id)).thenReturn(Optional.of(Test_List_2));
-		assertThat(this.service.readById(id)).isEqualTo(this.mapToDto(Test_List_2));
-		verify(this.repo, atLeastOnce()).findById(id);
-
+		ListName Test_List_2 = new ListName(id, "Tuesday");
+		when(this.mockRepo.findById(id)).thenReturn(Optional.of(Test_List_2));
+		assertThat(this.service.readById(id))
+		.isEqualTo(this.mapToDto(Test_List_2));
+		verify(this.mockRepo, atLeastOnce()).findById(id);
 	}
 
 	@Test
-	// NullPointerException - needs checking, all other tests are fine
 	void updateTEST() throws Exception {
 		final Long id = 1L;
-		when(this.repo.findById(id)).thenReturn(Optional.of(List_1));
-		when(this.repo.save(List_1_Gets)).thenReturn(List_1_Gets);
-		assertThat(this.service.update(this.mapToDto(List_1_Gets), id)).isEqualTo(this.mapToDto(List_1));
-		verify(this.repo, atLeastOnce()).findById(id);
-		verify(this.repo, atLeastOnce()).save(List_1_Gets);
+		when(this.mockRepo.findById(id)).thenReturn(Optional.of(List_1));
+		when(this.mockRepo.save(List_1_Gets)).thenReturn(List_1_Gets);
+		assertThat(this.service.update(this.mapToDto(List_1_Gets), id))
+		.isEqualTo(this.mapToDto(List_1));
+		verify(this.mockRepo, atLeastOnce()).findById(id);
+		verify(this.mockRepo, atLeastOnce()).save(List_1_Gets);
 	}
 
 	@Test
-	void deleteTEST() throws Exception {
+	void delete_branch_one_TEST() throws Exception {
 		final Long id = 2L;
-		when(this.repo.existsById(id)).thenReturn(false);
-		assertThat(this.service.delete(id)).isEqualTo(true);
-		verify(this.repo, atLeastOnce()).existsById(id);
-
+		when(this.mockRepo.existsById(id)).thenReturn(false);
+		assertThat(this.service.delete(id))
+		.isEqualTo(true);
+		verify(this.mockRepo, atLeastOnce()).existsById(id);
+	}
+	
+	@Test
+	void delete_branch_two_TEST() throws Exception {
+		// return !this.repo.existsById(id); pathway missed, next test objective
 	}
 }
