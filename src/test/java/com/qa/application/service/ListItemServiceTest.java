8cbbec42aaa91fd5302e54512871f6e0925a0b53
Peter Vaughan-Williams <pvaughan-williams@qa.com>
2021-02-20 15:50:41 +0000

    finished listitem service tests after backend fixes, all done except delete 2nd branch test

--- src/test/java/com/qa/application/service/ListItemServiceTest.java
+++ src/test/java/com/qa/application/service/ListItemServiceTest.java
@@ -25,69 +25,78 @@ import com.qa.application.persistence.repo.ListItemRepo;
 public class ListItemServiceTest {
 	
 	@Autowired
+	private ModelMapper mapper;
+	@Autowired
 	private ListItemService service;
 	@MockBean
-	private ListItemRepo repo;
-	@MockBean
-	private ModelMapper mockMapper;
+	private ListItemRepo mockRepo;
 	
-	private final ListItem Item_1 = new ListItem("Exercise","Chest Day", false);
-	private final ListItem Item_2 = new ListItem("Shopping","Lidl has the best deals", false);
-//	private final ListItem Item_1_Updated = new ListItem(1L, "Exercise More!","Chest and Back Day!!", false);
-//	private final ListItem Item_1_Gets = new ListItem(1L, Item_1_Updated.getName(), Item_1_Updated.getDescription(), false);
-	private final List<ListItem> listOfItemss = List.of(Item_1, Item_2);
+	private final ListItem Item_1 = new ListItem("Shopping","Groceries", true);
+	private final ListItem Item_2 = new ListItem("Exercise","push", true);
+	
+	private final ListItem Item_1_Updated = new ListItem(1L, "Shopping","Groceries and Technology", false);
+	private final ListItem Item_1_Gets = new ListItem(1L, Item_1_Updated.getName(), Item_1_Updated.getDescription(), true);
+	
+	private final List<ListItem> listOfItems = List.of(Item_1, Item_2);
 
 	private ListItemDto mapToDto(ListItem listItem) {
-		return this.mockMapper.map(listItem, ListItemDto.class);
+		return this.mapper.map(listItem, ListItemDto.class);
 	}
 	
 	@Test
 	void createTEST() throws Exception {
-		final ListItem Item_No_Id = new ListItem(null, "Exercise","Chest Day", false);
-		final ListItem Item_With_Id = new ListItem(1L, "Exercise","Chest Day", false);
-		when(this.repo.save(Item_No_Id)).thenReturn(Item_With_Id);
-		assertThat(this.service.create(Item_No_Id)).isEqualTo(this.mapToDto(Item_With_Id));
-		verify(this.repo, atLeastOnce()).save(Item_No_Id);
+		final ListItem Item_No_Id = new ListItem(null, "Shopping","Groceries", false);
+		final ListItem Item_With_Id = new ListItem(1L, "Shopping","Groceries", false);
+		when(this.mockRepo.save(Item_No_Id)).thenReturn(Item_With_Id);
+		assertThat(this.service.create(Item_No_Id))
+		.isEqualTo(this.mapToDto(Item_With_Id));
+		verify(this.mockRepo, atLeastOnce()).save(Item_No_Id);
 	}
 	
 	@Test
 	void readAllTEST() throws Exception {
-		List<ListItemDto> itemDtos = listOfItemss.stream().map(this::mapToDto).collect(Collectors.toList());
-		when(this.repo.findAll()).thenReturn(listOfItemss);
-		assertThat(this.service.readAll()).isEqualTo(itemDtos);
-		verify(this.repo, atLeastOnce()).findAll();
-
+		List<ListItemDto> itemDtos = listOfItems.stream().map(this::mapToDto).collect(Collectors.toList());
+		when(this.mockRepo.findAll()).thenReturn(listOfItems);
+		assertThat(this.service.readAll())
+		.isEqualTo(itemDtos);
+		verify(this.mockRepo, atLeastOnce()).findAll();
 	}
 	
 	@Test
 	void readByIdTEST() throws Exception {
 		final Long id = 2L;
-		ListItem Test_Item_2 = new ListItem("Shopping","Lidl has the best deals", false);
-		when(this.repo.findById(id)).thenReturn(Optional.of(Test_Item_2));
-		assertThat(this.service.readById(id)).isEqualTo(this.mapToDto(Test_Item_2));
-		verify(this.repo, atLeastOnce()).findById(id);
-
+		ListItem Test_Item_2 = new ListItem("Shopping","Groceries", true);
+		when(this.mockRepo.findById(id)).thenReturn(Optional.of(Test_Item_2));
+		assertThat(this.service.readById(id))
+		.isEqualTo(this.mapToDto(Test_Item_2));
+		verify(this.mockRepo, atLeastOnce()).findById(id);
 	}
-	
-//	@Test
-	// NullPointerException - needs checking, all other tests are fine
-//	void updateTEST() throws Exception {
-//		final Long id = 1L;
-//		when(this.repo.findById(id)).thenReturn(Optional.of(Item_1));
-//		when(this.repo.save(Item_1_Gets)).thenReturn(Item_1_Gets);
-//		assertThat(this.service.update(this.mapToDto(Item_1_Gets), id)).isEqualTo(this.mapToDto(Item_1));
-//		verify(this.repo, atLeastOnce()).findById(id);
-//		verify(this.repo, atLeastOnce()).save(Item_1_Gets);
-//	}
 	
 	@Test
-	void deleteTEST() throws Exception {
-		final Long id = 2L;
-		when(this.repo.existsById(id)).thenReturn(false);
-		assertThat(this.service.delete(id)).isEqualTo(true);
-		verify(this.repo, atLeastOnce()).existsById(id);
-
+	void updateTEST() throws Exception {
+		final Long id = 1L;
+		when(this.mockRepo.findById(id)).thenReturn(Optional.of(Item_1));
+		when(this.mockRepo.save(Item_1_Gets)).thenReturn(Item_1_Gets);
+		assertThat(this.service.update(this.mapToDto(Item_1_Gets), id))
+		.isEqualTo(this.mapToDto(Item_1));
+		verify(this.mockRepo, atLeastOnce()).findById(id);
+		verify(this.mockRepo, atLeastOnce()).save(Item_1_Gets);
 	}
+	
+	@Test
+	void delete_branch_one_TEST() throws Exception {
+		final Long id = 2L;
+		when(this.mockRepo.existsById(id)).thenReturn(false);
+		assertThat(this.service.delete(id))
+		.isEqualTo(true);
+		verify(this.mockRepo, atLeastOnce()).existsById(id);
+	}
+	
+	@Test
+	void delete_branch_two_TEST() throws Exception {
+		// return !this.repo.existsById(id) - missing second branch here; need to test 		
+	}
+
 
 
 }
