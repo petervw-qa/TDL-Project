1ed6cbea2336eb7d715fb90bbd694966f47d7f18
Peter Vaughan-Williams <pvaughan-williams@qa.com>
2021-02-20 13:53:46 +0000

    edited app-schema and app-data to fix integration tests

--- src/test/resources/application-data.sql
+++ src/test/resources/application-data.sql
@@ -1,6 +1,6 @@
-insert into list_name (name) values ('Peter List');
-insert into list_name (name) values ('Mason List');
-insert into list_name (name) values ('Artur List');
-insert into list_items (name, description, completed, listname_id) values('Groceries Shop', 'Buy some chicken', false, 1);
-insert into list_items (name, description, completed, listname_id) values('Build PC', 'Put together new components' , true , 1);
-insert into list_items (name, description, completed, listname_id) values('Complete TDL Project', 'Still need to finish front end', false, 1);
+insert into list_name (name) values ('Monday');
+insert into list_name (name) values ('Tuesday');
+insert into list_name (name) values ('Wednesday');
+insert into list_item (name, description, completed, listname_id) values('Shopping', 'Groceries', true, 1);
+insert into list_item (name, description, completed, listname_id) values('Exercise', 'Push' , true , 1);
+insert into list_item (name, description, completed, listname_id) values('Cook', 'Chicken', true, 1);
--- src/test/resources/application-schema.sql
+++ src/test/resources/application-schema.sql
@@ -1,4 +1,5 @@
 drop table if exists `list_name` CASCADE;
-drop table if exists `list_items` CASCADE;
+drop table if exists `list_item` CASCADE;
 create table list_name (id bigint PRIMARY KEY AUTO_INCREMENT, name varchar(255) not null);
-create table list_items (id bigint PRIMARY KEY AUTO_INCREMENT, completed boolean not null, description varchar(255) not null, name varchar(255) not null, listname_id bigint);
+create table list_item (id bigint PRIMARY KEY AUTO_INCREMENT, completed boolean not null, description varchar(255) not null, name varchar(255) not null, listname_id bigint);
+alter table list_item add constraint FK6ikpg1bdaaa05dflt1e84kt63 foreign key (listname_id) references list_name on delete cascade;
